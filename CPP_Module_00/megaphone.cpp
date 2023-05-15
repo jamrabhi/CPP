bd2de2385fabfdb3a55ad8f34f299e24e82ad914
Jamal RABHIOUAN <jamrabhi@e3r8p13.clusters.42paris.fr>
2023-05-15 20:17:16 +0200

    save progress

--- CPP_Module_00/megaphone.cpp
+++ CPP_Module_00/megaphone.cpp
@@ -1,19 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.cpp                                           :+:      :+:    :+:   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jamrabhi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/10 21:54:03 by jamrabhi          #+#    #+#             */
-/*   Updated: 2023/05/10 21:54:06 by jamrabhi         ###   ########.fr       */
+/*   Created: 2023/05/15 19:44:39 by jamrabhi          #+#    #+#             */
+/*   Updated: 2023/05/15 19:44:42 by jamrabhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cctype>
 
 int	main(int ac, char *av[])
 {
-	std::cout << "Hello world!" << std::endl;
+	if (ac > 1)
+	{
+		for (int i = 0; i < ac; i++)
+			std::cout << av[i + 1];
+	}
+	else
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	}
+	std::cout << std::endl;
 	return (0);
 }
