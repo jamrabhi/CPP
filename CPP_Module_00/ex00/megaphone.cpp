f069a8086a1ec75dcd5473535e53e6ee41d4e2ec
jamrabhi-WSL <jamrabhi@student.42.fr>
2023-05-16 04:49:02 +0200

    finshed ex00

--- CPP_Module_00/ex00/megaphone.cpp
+++ CPP_Module_00/ex00/megaphone.cpp
@@ -16,14 +16,11 @@
 int	main(int ac, char *av[])
 {
 	if (ac > 1)
-	{
-		for (int i = 0; i < ac; i++)
-			std::cout << av[i + 1];
-	}
+		for (int i = 1; i < ac; i++)
+			for (int j = 0; av[i][j]; j++)
+				std::cout << (char)toupper(av[i][j]);
 	else
-	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	}
 	std::cout << std::endl;
 	return (0);
 }
