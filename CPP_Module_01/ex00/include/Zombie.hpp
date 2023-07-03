b397e72efb7947e79d14e750346c901ae38253cf
jamrabhi-WSL <jamrabhi@student.42.fr>
2023-07-03 02:01:38 +0200

    save progress

---
+++ CPP_Module_01/ex00/include/Zombie.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jamrabhi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/02 16:53:50 by jamrabhi          #+#    #+#             */
+/*   Updated: 2023/07/02 16:53:51 by jamrabhi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <string>
+
+class Zombie
+{
+	public:
+
+		Zombie();
+		~Zombie();
+		void announce(void);
+
+	private:
+
+		std::string name;
+};
+
+#endif
