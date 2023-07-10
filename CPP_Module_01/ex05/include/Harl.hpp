47a2a21fdaac2f7e72606c4319938f6d21873d0f
jamrabhi <jamrabhi@student.42.fr>
2023-07-10 04:03:27 +0200

    save progress CPP01_ex05

---
+++ CPP_Module_01/ex05/include/Harl.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jamrabhi <jamrabhi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/10 03:11:58 by jamrabhi          #+#    #+#             */
+/*   Updated: 2023/07/10 03:12:11 by jamrabhi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <iostream>
+# include <string>
+
+class Harl
+{
+	public:
+		Harl();
+		~Harl();
+		void	complain(std::string level);
+
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+};
+
+#endif
---
+++ CPP_Module_01/ex05/src/Harl.cpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jamrabhi <jamrabhi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/10 03:12:24 by jamrabhi          #+#    #+#             */
+/*   Updated: 2023/07/10 03:12:26 by jamrabhi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl()
+{
+}
+
+Harl::~Harl()
+{
+}
+
+void	Harl::complain(std::string level)
+{
+
+}
+
+void	Harl::debug(void)
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-\
+		pickle-special-ketchup burger. I really do!" << std::endl;
+}
+
+void	Harl::info(void)
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. \
+		You didn’t put enough bacon in my burger! If you did, I wouldn’t \
+		be asking for more!" << std::endl;
+}
+
+void	Harl::warning(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free. \
+		I’ve been coming for years whereas you started working here since \
+		last month." << std::endl;
+}
+
+void	Harl::error(void)
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now."
+		<< std::endl;
+}
