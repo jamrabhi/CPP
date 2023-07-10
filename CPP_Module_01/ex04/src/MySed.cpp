e79aca875a95733f3f254cae50e7a132ec8108d6
jamrabhi <jamrabhi@student.42.fr>
2023-07-10 03:05:55 +0200

    finished CPP01_ex04

--- CPP_Module_01/ex04/src/MySed.cpp
+++ CPP_Module_01/ex04/src/MySed.cpp
@@ -23,6 +23,11 @@ MySed::~MySed()
 
 int	MySed::replace()
 {
+	if(_s1.length() == 0)
+	{
+		std::cerr << "Error : <s1> must have at least 1 character" << std::endl;
+		return (1);
+	}
 	std::ifstream infile(_filename.c_str());
 
 	if (!infile.is_open())
@@ -31,32 +36,26 @@ int	MySed::replace()
 			<< "\" doesn't exist or cannot be opened" << std::endl;
 		return (1);
 	}
-	else
+	std::string		outfile_name = _filename + ".replace";
+	std::ofstream	outfile(outfile_name.c_str());
+	std::string		line;
+	
+	if (!outfile.is_open())
 	{
-		std::string		outfile_name = _filename + ".replace";
-		std::ofstream	outfile(outfile_name.c_str());
-		
-		if (!outfile.is_open())
-		{
-			std::cerr << "Error : couldn't save .replace file" << std::endl;
-			return (1);
-		}
-		else
-		{
-			std::string	line;
-
-			while(std::getline(infile, line))
-			{
-				for (size_t found = line.find(_s1); found != std::string::npos; found = line.find(_s1, found))
-				{
-					line.erase(found, _s1.length());
-					line.insert(found, _s2);
-				}
-				outfile << line << std::endl;
-			}
-		}
-		outfile.close();
-		infile.close();
+		std::cerr << "Error : couldn't save .replace file" << std::endl;
+		return (1);
 	}
+	while(std::getline(infile, line))
+	{
+		for (size_t found = line.find(_s1); found != std::string::npos;
+				found = line.find(_s1, found))
+		{
+			line.erase(found, _s1.length());
+			line.insert(found, _s2);
+		}
+		outfile << line << std::endl;
+	}
+	outfile.close();
+	infile.close();
 	return (0);
 }
