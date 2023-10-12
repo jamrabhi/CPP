90a1ba40a90a621e144792bd1d7916a2fc9304f7
Jamal RABHIOUAN <jamrabhi@made-f0Br8s10.clusters.42paris.fr>
2023-10-12 21:58:31 +0200

    save cpp06/ex00 : check ./convert -214748359

--- CPP_Module_06/ex00/src/ScalarConverter.cpp
+++ CPP_Module_06/ex00/src/ScalarConverter.cpp
@@ -6,12 +6,13 @@
 /*   By: jamrabhi <jamrabhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 18:57:41 by jamrabhi          #+#    #+#             */
-/*   Updated: 2023/10/10 22:33:04 by jamrabhi         ###   ########.fr       */
+/*   Updated: 2023/10/12 21:56:55 by jamrabhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 #include <sstream>
+#include <iomanip>
 
 /* ************************************************************************** */
 /* 							CONSTRUCTORS / DESTRUCTORS						  */
@@ -48,15 +49,34 @@ ScalarConverter	&ScalarConverter::operator=(ScalarConverter const &rhs)
 
 void	ScalarConverter::convert(std::string str)
 {
-	std::istringstream newstr(str);
-	int myint;
-	newstr >> myint;
-	// if (str.length() == 1)
-	// {
-		std::cout << "char : '" << static_cast<char>(myint) << "' " << std::endl;
-	// }
-	// else
-	// {
-		std::cout << "char : impossible" << std::endl;
-	// }
+	if (str.length() == 1 && std::isprint(str[0]) && !isdigit(str[0]))
+	{
+		std::istringstream	newstr(str);
+		char				char_conversion;
+
+		newstr >> char_conversion;
+		
+		std::cout << std::fixed << std::setprecision(1);
+		std::cout << "char: '" << static_cast<char>(char_conversion) << "' " << std::endl;
+		std::cout << "int: " << static_cast<int>(char_conversion) << std::endl;
+		std::cout << "float: " << static_cast<float>(char_conversion) << "f" << std::endl;
+		std::cout << "double: " << static_cast<double>(char_conversion) << std::endl;
+
+	}
+	else
+	{
+		std::istringstream	newstr(str);
+		int				int_conversion;
+
+		newstr >> int_conversion;
+		
+		std::cout << std::fixed << std::setprecision(1);
+		if (std::isprint(static_cast<char>(int_conversion)))
+			std::cout << "char: '" << static_cast<char>(int_conversion) << "' " << std::endl;
+		else
+			std::cout << "char: Non displayable" << std::endl;
+		std::cout << "int: " << static_cast<int>(int_conversion) << std::endl;
+		std::cout << "float: " << static_cast<float>(int_conversion) << "f" << std::endl;
+		std::cout << "double: " << static_cast<double>(int_conversion) << std::endl;
+	}
 }
