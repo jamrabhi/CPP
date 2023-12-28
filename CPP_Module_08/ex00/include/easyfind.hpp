084280d75e72bfbb384309b5b73b9883736bccb3
Jamal RABHIOUAN <jamrabhi@made-f0Dr7s12.clusters.42paris.fr>
2023-12-28 21:21:05 +0100

    start cpp08/ex00

---
+++ CPP_Module_08/ex00/include/easyfind.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jamrabhi <jamrabhi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/28 18:44:26 by jamrabhi          #+#    #+#             */
+/*   Updated: 2023/12/28 21:20:03 by jamrabhi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <iostream>
+# include <string>
+
+template<typename T>
+void	easyfind(T &container, int n)
+{
+		
+}
+
+#endif
