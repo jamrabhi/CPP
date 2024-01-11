06ab1a52371fabc11a72dbe4c71d146d858c4b89
Jamal RABHIOUAN <jamrabhi@made-f0Dr3s6.clusters.42paris.fr>
2024-01-11 21:20:49 +0100

    finish cpp08/ex00

--- CPP_Module_08/ex00/include/easyfind.hpp
+++ CPP_Module_08/ex00/include/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: jamrabhi <jamrabhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 18:44:26 by jamrabhi          #+#    #+#             */
-/*   Updated: 2024/01/02 20:33:53 by jamrabhi         ###   ########.fr       */
+/*   Updated: 2024/01/11 21:14:16 by jamrabhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <iostream>
 # include <string>
 # include <algorithm>
+# include <stdexcept>
 
 template<typename T>
 typename T::iterator	easyfind(T &container, int n)
@@ -23,10 +24,8 @@ typename T::iterator	easyfind(T &container, int n)
 	typename T::iterator	it;
 
 	it = find(container.begin(), container.end(), n);
-	if (it != container.end())
-		std::cout << "found" << std::endl;
-	else
-		std::cout << "Not found !!!!" << std::endl;
+	if (it == container.end())
+		throw std::out_of_range("Value not found");
 	return (it);
 }
 
--- CPP_Module_08/ex00/src/main.cpp
+++ CPP_Module_08/ex00/src/main.cpp
@@ -6,24 +6,84 @@
 /*   By: jamrabhi <jamrabhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 18:43:22 by jamrabhi          #+#    #+#             */
-/*   Updated: 2024/01/02 20:34:53 by jamrabhi         ###   ########.fr       */
+/*   Updated: 2024/01/11 21:20:00 by jamrabhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
-#include <map>
+#include <vector>
+#include <list>
 
 int	main()
 {
-	std::map<const int, int> mymap;
-	std::map<const int, int>::iterator it;
+	int myints[] = {9, 42, 5, 212};
+	
+	// Creation of my vector
+	std::vector<int> v(myints, myints + sizeof(myints) / sizeof(int));
 
-	mymap[0] = 42;
-	mymap[1] = 24;
-	mymap[2] = 94;
+	std::cout << "Content of my vector :";
+	for (std::vector<int>::iterator it = v.begin(); it != v.end(); it++)
+	{
+		std::cout << ' ' << *it;
+	}
+	std::cout << std::endl << std::endl;
 
-	it = easyfind(mymap, 1);
-	// std::cout << it->second << std::endl;
+	// Testing easyfind with my vector
+	std::vector<int>::iterator v_it;
+	std::cout << "Trying to find value 42 in my vector" << std::endl;
+	try
+	{
+		v_it = easyfind(v, 42);
+		std::cout << "42 was found" << std::endl;
+	}
+	catch(const std::exception &e)
+	{
+		std::cerr << "Error : " << e.what() << std::endl;
+	}
+	std::cout << "Trying to find value 21 in my vector" << std::endl;
+	try
+	{
+		v_it = easyfind(v, 21);
+		std::cout << "Value found" << std::endl;
+	}
+	catch(const std::exception &e)
+	{
+		std::cerr << "Error : " << e.what() << std::endl;
+	}
+	std::cout << std::endl << std::endl;
 
+	// Creation of my list
+	std::list<int> l(myints, myints + sizeof(myints) / sizeof(int));
+
+	std::cout << "Content of my list :";
+	for (std::list<int>::iterator it = l.begin(); it != l.end(); it++)
+	{
+		std::cout << ' ' << *it;
+	}
+	std::cout << std::endl << std::endl;
+
+	// Testing easyfind with my list
+	std::list<int>::iterator l_it;
+	std::cout << "Trying to find value 212 in my list" << std::endl;
+	try
+	{
+		l_it = easyfind(l, 212);
+		std::cout << "212 was found" << std::endl;
+	}
+	catch(const std::exception &e)
+	{
+		std::cerr << "Error : " << e.what() << std::endl;
+	}
+	std::cout << "Trying to find value 0 in my list" << std::endl;
+	try
+	{
+		l_it = easyfind(l, 0);
+		std::cout << "Value found" << std::endl;
+	}
+	catch(const std::exception &e)
+	{
+		std::cerr << "Error : " << e.what() << std::endl;
+	}
+	
 	return (0);
 }
